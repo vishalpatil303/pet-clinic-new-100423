19b6db50d577928ebd17201fafe6bbb7d09ee212
vishalpatil303 <vishal.patil303@gmail.com>
2023-04-17 13:02:07 +0530

    Vet Controller added

--- pet-clinic-web/src/main/java/com/petclinc/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/petclinc/controller/VetController.java
@@ -1,9 +1,25 @@
 package com.petclinc.controller;
 
+import com.petclinc.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vet")
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping({"/vets","/vets/index","/"})
+    public String getVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
+        model.addAttribute("title","Vet List");
+        return "vet";
+    }
 }
